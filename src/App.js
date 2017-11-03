e208d9b782a932e0c0a9cd26ea3fe7cd80bf32c0
Edmond Atto <edmond091@gmail.com>
2017-11-03 07:50:27 +0300

    Add componentDidMount method that fetches data from the giphy API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
-
+import axios from 'axios';
 export default class App extends Component {
   
   constructor() {
     super();
-  } 
+    this.state = {
+      gifs: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.giphy.com/v1/gifs/trending?api_key=pEC4gYDzDgd16U9oZVONW9uY6JVqAblU&limit=25&rating=G')
+      .then(response => {
+        this.setState({
+          gifs: response.data.data,
+        })
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error)
+      })
+  }
 
   render() { 
     return (
